980dd99db5594745a51eec820f3a99550c785a70
KEERTHIVASAN K <109860552+MADHACKER85@users.noreply.github.com>
2023-11-09 15:12:24 +0530

    Add files via upload

---
+++ network_scanner.py
@@ -0,0 +1,49 @@
+##This is a network scanner which works over either individual IPs or ranges.
+##This uses `subprocess` to run the required commands in the Linux Shell.
+##It then uses `re` to check if the MAC address has been changed correctly.
+##This version uses the `argparse` library instead of `optparse`.
+
+
+
+import scapy.all as scapy
+import argparse
+
+def header():
+	print('''
+===========[ N e t w o r k  s c a n n e r ]===========
+Written by Substing, assignmnet 1 from a course by ZSecurity.
+		''')
+
+def get_argument():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-t", "--target", dest ="target", help = "Target adress or adress range to scan")
+	option = parser.parse_args()
+	if not option.target:
+		parser.error("[-] Please specify a target address to scan --help for more info.")
+
+	return option.target
+
+
+def scan(ip):
+	arp_request = scapy.ARP(pdst = ip)
+	broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+	arp_request_broadcast = broadcast/arp_request
+	answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+	clients_list = []
+
+	for element in answered_list:
+		client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+		clients_list.append(client_dict)
+	return(clients_list)
+
+def print_result(results_list):
+	print("IP\t\t\tMAC Address\n-----------------------------------------------")
+	for client in results_list:
+		print(client["ip"] + "\t\t" + client["mac"])
+
+
+header()
+ip = get_argument()
+scan_result = scan(ip)
+print_result(scan_result)
